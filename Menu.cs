dc8736f2d984011d365997083965518e66d63f55
Alexander Eide <atlexeide@gmail.com>
2024-11-19 13:57:26 +0100

    Update Menu class

--- Menu.cs
+++ Menu.cs
@@ -1,8 +1,17 @@
+using System.Numerics;
+using static System.Console;
 public class Menu
 {
-    public static Options Options = new Options();
+    Options Options = new Options();
+
     public Menu()
     {
-    
+
+    }
+    public void Open()
+    {
+        Clear();
+        WriteLine("A menu");
+        Options.ListOptions();
     }
 }
