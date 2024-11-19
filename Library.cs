afa3d3fe4c7845a296c91ec5e0455c93a06225b9
Alexander Eide <atlexeide@gmail.com>
2024-11-19 13:00:42 +0100

    Add Library class

---
+++ Library.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.Design;
+
+public class Library
+{
+    String Name;
+    static List<Book> Books;
+    static Menu Menu;
+    public Library(String _Name)
+    {
+        Name = _Name;
+    }
+}
