e687d0200a795158f5526271e7b173d06cf5eb21
Alexander Eide <atlexeide@gmail.com>
2024-11-19 12:57:25 +0100

    Add Book class

---
+++ Book.cs
@@ -0,0 +1,12 @@
+public class Book
+{
+    String Author;
+    String Title;
+    int Pages;
+    public Book(String _Author, String _Title, int _Pages)
+    {
+        Author = _Author;
+        Title =  _Title;
+        Pages = _Pages;
+}
+};
