c0c141eb8335abc86e87fab3489294d65bf2410d
Alexander Eide <atlexeide@gmail.com>
2024-11-19 13:57:35 +0100

    Update Options class

--- Options.cs
+++ Options.cs
@@ -1,8 +1,17 @@
+using static System.Console;
 public class Options
 {
-    
+    List<String> OptionList = new List<String>();
     public Options()
     {
-
+        OptionList.Add("List books");
+        OptionList.Add("Lend a book");
+        OptionList.Add("Add book");
+    }
+    public void ListOptions(){
+        foreach (String Option in OptionList)
+        {
+            WriteLine(Option);
+        }
     }
 }
